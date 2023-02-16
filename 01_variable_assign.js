8dc6eccec3a693fd74301d77c9a3f0eb4a3e2f0a
Vishal Dudhabade <vishaldudhabade2329@gmail.com>
2023-02-16 12:59:51 +0530

    My first JavaScript Assignment
    My first JavaScript Assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,40 @@
+console.log("******** Step-1 *********");
+console.log("......Variable value before initilization......");
+var myFullName;     // Variable Declaration
+console.log(myFullName);
+
+myFullName = "My Full Name:- Vishal Dudhabade";    // Initilization 
+console.log(myFullName);
+
+myFullName = "My Full Name:- Vishal Niranjan Dudhabade";  // Update
+console.log(myFullName);
+
+console.log("********** Step-2 ***********");
+var myLove = "I love only JS";
+console.log(myLove);
+
+console.log("********** Step-3 ***********")
+var firstName = "First Name:- Vishal";
+console.log(firstName);
+
+var lastName = "Last Name:- Dudhabade";
+console.log(lastName);
+
+age = "Age:- 26";
+console.log(age);
+
+address = "Address:- Nagpur";
+console.log(address);
+
+pincode = "Pincode:- 441110"
+console.log(pincode);
+
+gender = "Gender:- Male";
+console.log(gender);
+
+isMarried = "Married Status:- Unmarried"
+console.log(isMarried);
+
+
+
+
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,27 @@
-console.log( "Mohit Sharma ");
+console.log( "Mohit Sharma "); 
+console.log("25");
+console.log("Nagpur");
+
+var fullName = "Vishal Dudhabade";
+console.log(fullName);
+console.log(fullName);
+
+var city;   // This is Variable Declaration
+console.log(city);  // This code output show undefined because variable is present but value is not present
+
+city = "Pune"  // This is Variable Initialization 
+console.log(city);
+
+city = "Nagpur"   // variable update
+console.log(city)
+
+
+
+console.log("*************Details************")  // This type we can give the Heading
+console.log(" My full Name");
+var lastName = "Dudhabade";
+var education = "BE"
+var cast = "OBC"
+console.log(lastName)
+console.log(education)
+console.log(cast)
